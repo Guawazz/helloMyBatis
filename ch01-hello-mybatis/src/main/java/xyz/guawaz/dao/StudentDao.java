a676829a4ffd1a0d3ae9afd40363aae03c831d28
Guawazz <956014351@qq.com>
2021-07-24 00:33:35 +0800

    dao and proxy dao

--- ch01-hello-mybatis/src/main/java/xyz/guawaz/dao/StudentDao.java
+++ ch01-hello-mybatis/src/main/java/xyz/guawaz/dao/StudentDao.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface StudentDao {
     //查询Student表的所有数据
     public List<Student> selectStudents();
+
+    //向数据库插入数据
+    public int insertStudent(Student student);
 }
--- ch01-hello-mybatis/src/test/java/xyz/guawaz/AppTest.java
+++ ch01-hello-mybatis/src/test/java/xyz/guawaz/AppTest.java
@@ -2,19 +2,51 @@ package xyz.guawaz;
 
 import static org.junit.Assert.assertTrue;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+import xyz.guawaz.domain.Student;
 
-/**
- * Unit test for simple App.
- */
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test1() throws IOException {
+        //1.定义mybatis主配置文件的名称
+        String config = "mybatis.xml";
+        //2.读取这个config表示的文件
+        InputStream in = Resources.getResourceAsStream(config);
+        //3.创建SqlSessionFactoryBuilder对象
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        //4.创建SqlSessionFactory对象
+        SqlSessionFactory factory = builder.build(in);
+        //5.【重要】获取SqlSession对象，从SqlSessionFactory中获取SqlSession
+        SqlSession sqlSession = factory.openSession();
+        //6.【重要】指定要执行的sql语句的标识。sql映射文件中的namespace + "." + 标签的id值
+        String sqlId = "xyz.guawaz.dao.StudentDao.insertStudent";
+        //7.执行sql语句，通过sqlId找到语句
+        Student stu = new Student();
+         stu.setId(1007);
+         stu.setName("JayChou");
+         stu.setEmail("Jay@qq.com");
+         stu.setAge(33);
+        int nums = sqlSession.insert(sqlId,stu);
+
+        //提交事务,mybatis默认不是自动提交事务的
+        sqlSession.commit();
+
+
+        //8.输出结果
+        System.out.println("成功将"+nums+"条学生信息插入student表");
+        //9.关闭sqlSession对象
+        sqlSession.close();
     }
+
 }
